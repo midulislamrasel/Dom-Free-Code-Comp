22320ad290cf9ff53f66f2b7601a476cae96df93
Midul-islam-Russell <midulraddy121@gmail.com>
2022-09-17 20:12:04 +0600

    project02 End

--- Project02/index.js
+++ Project02/index.js
@@ -6,3 +6,21 @@
 **CSS Animation
 */
 
+let openBtn = document.getElementById('open-btn');
+let modalContainer = document.getElementById('modal-container');
+let closeBtn = document.getElementById('close-btn');
+
+//Even Listeners
+openBtn.addEventListener('click', function () {
+    modalContainer.style.display = 'block';
+});
+
+closeBtn.addEventListener('click', function () {
+    modalContainer.style.display = 'none';
+});
+
+window.addEventListener('click', function (e) {
+    if (e.target === modalContainer) {
+        modalContainer.style.display = 'none';
+    }
+});
