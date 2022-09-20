0255c90c7b5d555b32b676bee124a30c1229e282
Midul-islam-Russell <midulraddy121@gmail.com>
2022-09-20 21:22:10 +0600

    JavaScript

--- Project05/main.js
+++ Project05/main.js
@@ -8,3 +8,44 @@
             **remove()
             **classList.add()
  */
+
+
+//Variables
+
+const addTask = document.getElementById('add-task');
+const taskContainer = document.getElementById('task-container');
+const inputTask = document.getElementById('input-task');
+
+//Event Listener For add Button
+addTask.addEventListener('click', function () {
+    let task = document.createElement('div');
+    task.classList.add('text');
+
+    let li = document.createElement('li');
+    li.innerText = `${inputTask.value}`;
+    task.appendChild(li)
+
+
+    let checkButton = document.createElement("button");
+    checkButton.innerHTML = '<i class = "fa-solid fa-check"></i>';
+    checkButton.classList.add('checkTask');
+    task.appendChild(checkButton);
+
+
+    //delete
+
+    let deleteButton = document.createElement("button");
+    deleteButton.innerHTML = '<i class = "fa-solid fa-trash-can"></i>';
+    checkButton.classList.add('deleteTask');
+    task.appendChild(deleteButton);
+
+
+    if (inputTask.value === " ") {
+        alert('Please Enter a Task');
+    }
+    else {
+        taskContainer.appendChild(task);
+    }
+
+    inputTask.value = "";
+})
